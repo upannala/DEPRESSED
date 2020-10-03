d427a2e3d11f82ccda9c888f8c397521f0aaf155
upannala <40683659+upannala@users.noreply.github.com>
2020-10-03 07:10:34 +0530

    Update video.py

--- video.py
+++ video.py
@@ -60,10 +60,7 @@ def eye_aspect_ratio(eye):
 
 detector = dlib.get_frontal_face_detector()
 #load model
-#model = model_from_json(open("model.json", "r").read())
-
-model_json = model.to_json()
-open('model.json', 'w').write(model_json)
+model = model_from_json(open("model.json", "r").read())
 
 #load weights
 model.load_weights('model.h5')
